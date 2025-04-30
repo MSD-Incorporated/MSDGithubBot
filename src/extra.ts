e84044cdc7f8915968ed0e205ded08ee8b157bfe
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-04-30 22:13:24 +0500

    little changes

--- src/extra.ts
+++ src/extra.ts
@@ -1,3 +1,10 @@
 import type { UserFromGetMe } from "grammy/types";
 
 export const onStart = ({ username, id }: UserFromGetMe) => console.log(`${username} [${id}] started`);
+
+export const githubRegexLink =
+	/(?:https?:\/\/)?(?:www\.)?(?:github)\.com\/(?<repo>[a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/blob\/(?<path>.+?)#L(?<first_line_number>\d+)[-~]?L?(?<second_line_number>\d*)/i;
+
+export function safeSlice<T extends string | Array<any>>(input: T, length: number): T {
+	return <T>(input.length > length ? input.slice(0, length) : input);
+}
--- src/github.ts
+++ src/github.ts
@@ -1,12 +1,7 @@
 import { Composer, InlineKeyboard } from "grammy";
+import { githubRegexLink, safeSlice } from "./extra";
 
 export const githubComposer = new Composer();
-const githubRegexLink =
-	/(?:https?:\/\/)?(?:www\.)?(?:github)\.com\/(?<repo>[a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/blob\/(?<path>.+?)#L(?<first_line_number>\d+)[-~]?L?(?<second_line_number>\d*)/i;
-
-export function safeSlice<T extends string | Array<any>>(input: T, length: number): T {
-	return <T>(input.length > length ? input.slice(0, length) : input);
-}
 
 // Thanks to https://github.com/xhyrom/bun-discord-bot
 githubComposer.hears(githubRegexLink, async ctx => {
