d5f0906fe5baad2e3025e84355abca54f5ee564b
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-04-30 22:21:07 +0500

    Docker

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { Bot } from "grammy";
 import { onStart } from "./extra";
 import { githubComposer } from "./github";
 
-const TOKEN = process.env.TOKEN;
-const client = new Bot(TOKEN);
+const client = new Bot(process.env.TOKEN, { client: { apiRoot: process.env.LOCAL_API ?? "https://api.telegram.org" } });
 
 client.use(autoQuote());
 client.use(githubComposer);
